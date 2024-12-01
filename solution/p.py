b5ce589cb34811c9a74578e5fdf94dec2233b87c
superiorshrimp <szymon.zychowicz300701@gmail.com>
2024-12-01 17:00:46 +0100

    good enough

--- solution/p.py
+++ solution/p.py
@@ -1,19 +1,19 @@
 from numpy.linalg import solve
 import numpy as np
+from matplotlib import pyplot as plt
 
 
-def get_phi(u, i, n): return 0.5 * (u[i,n] + u[i-1,n]) * (u[i,n] - u[i-1,n])
+def get_phi(u, i, n): return 0.5 * (u[i] + u[i-1]) * (u[i] - u[i-1])
 
-def get_F(u, i, n): return 0.5 * (u[i,n]**2)
+def get_F(u, i, n): return 0.5 * (u[i]**2)
 
-def get_b(n, u):
-    w = 0.5
-    b = [0]
-    for i in range(1,n-1):
-        b_i = u[i,n] - 0.5 * (dt/dx) * (get_F(u, i+1, n) - get_F(u, i, n)) + 0.5*w * (dt/dx) * (get_phi(u, i+1, n) - get_phi(u, i-1, n)) # phi j
-        b.append(b_i)
+def get_b(n, u, w):
+    # zakładam, że b[0] i b[-1] = 0 i i=j; TODO: nk zweryfikuje
+    b = [0] + [
+        u[i] - 0.5 * (dt/dx) * (get_F(u, i+1, n) - get_F(u, i, n)) + 0.5*w * (dt/dx) * (get_phi(u, i+1, n) - get_phi(u, i-1, n)) for i in range(1,n-1)
+    ] + [0]
 
-    return np.array(b + [0])
+    return np.array(b)
 
 def get_A(n, r):
     A = np.zeros((n,n))
@@ -33,19 +33,41 @@ def get_A(n, r):
 
 def get_r(mu, dt, dx): return mu * dt / (dx*dx)
 
-def main(n, mu, dt, dx):
-    r = get_r(mu, dt, dx)
+def get_init_u(n, dx, a, mu):
+    x = np.array([np.pi * i * dx for i in range(n)])
 
-    u = np.zeros(n)
-    A = get_A(n, r)
-    print(A)
-    # b = get_b(n, u)
-    # u = np.linalg.solve(A, b)
+    return (2 * mu * np.pi) * np.sin(x) / (a + np.cos(x))
+
+def main(n, mu, dt, dx, w=0.5, a=8):
+    u = get_init_u(n, dx, a, mu)
+    aggregated_result = [u]
     print(u)
+
+    r = get_r(mu, dt, dx)
+    for _ in range(1,2*n): # time steps
+        A = get_A(n, r)
+        print(A)
+        b = get_b(n, u, w)
+        print(b)
+        u = np.linalg.solve(A, b)
+        print(u)
+        aggregated_result.append(u)
+
+    return np.array(aggregated_result)
+
+def plot_result(result):
+    fig = plt.figure()
+    ax = plt.axes(projection="3d")
+    y = np.arange(len(result))
+    x = np.arange(len(result[0]))
+    (x,y) = np.meshgrid(x,y)
+    ax.plot_surface(x,y,result)
+    plt.show()
 
 if __name__ == "__main__":
     n = 10
     mu = 0.5
     dt = 1/n
     dx = 1/n
-    main(n, mu, dt, dx)
+    result = main(n, mu, dt, dx)
+    plot_result(result)
