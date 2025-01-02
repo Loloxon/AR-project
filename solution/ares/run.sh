a96cf7159d1541c3a41607dc765bf2d98d2be7d4
superiorshrimp <szymon.zychowicz300701@gmail.com>
2025-01-02 21:10:48 +0100

    run.sh fix

--- solution/ares/run.sh
+++ solution/ares/run.sh
@@ -12,6 +12,18 @@
 
 module load openmpi
 module load python
-mpicc -lm -ldl -o $HOME/AR/pro/main $HOME/AR/pro/main.c -D SIDE_LENGTH=23 -D ITERATIONS=650
-mpiexec -np 12 $HOME/AR/pro/main
-python $HOME/AR/pro/heatmap.py  $HOME/AR/pro/out.csv
+
+pip install -q --upgrade pip
+pip install -q pandas
+pip install -q numpy
+pip install -q matplotlib
+pip install -q seaborn
+pip install -q imageio
+pip install -q tqdm
+
+# TODO: tutaj sobie ustawcie swoją ścieżkę:
+export P="$HOME/ar/AR-project/solution/ares"
+
+mpicc -lm -ldl -o "$P/main" "$P/main.c" -D SIDE_LENGTH=23 -D ITERATIONS=650
+mpiexec -np 12 "$P/main"
+python "$P/heatmap.py"  "$P/out.csv"
